46bb41bff5c10d97274b1976025f1916e94bd2c7
Alex <oleksandr.levchenko2001@ukr.net>
2020-12-29 21:23:33 +0200

    init

---
+++ Lab_3/Lab_3/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Lab_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите число : ");
+            int n = int.Parse(Console.ReadLine());
+            int a = n / 100;
+            int c = n % 10;
+            int b = n % 100 / 10;
+            if (a == b || b == c || a == c)
+            {
+                Console.WriteLine("Некоторые цифры трехзначного числа одинаковые");
+            }
+            else
+            {
+                Console.WriteLine("Все цифры трехзначного числа не одинаковые");
+            }
+        }
+    }
+}
