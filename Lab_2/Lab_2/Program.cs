5b36654b412f8bd36aeaca27e4aab28d77ca2484
Alex <oleksandr.levchenko2001@ukr.net>
2020-12-30 11:25:20 +0200

    init

---
+++ Lab_2/Lab_2/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Lab_2
+{
+    class Program
+    {
+        static void Main()
+        {
+           
+            {
+                double n, k;
+                Console.WriteLine("Enter (n) and (k) ");
+                n = int.Parse(Console.ReadLine());
+                k = int.Parse(Console.ReadLine());
+
+                double u = 1;
+
+                for (int i = 0; i < k; i++)
+                {
+                    u *= (3 * k + Math.Pow(-1, ((Math.Pow(k, 2)) - k + 1)) * k) / (2 * Math.Pow(k, 2) + 1);
+                }
+                Console.WriteLine("u=" + u);
+            }
+
+
+        }
+    }
+
+}
