8f2d68712a678489fec3b9395710319aa528ab1d
Alex <oleksandr.levchenko2001@ukr.net>
2020-12-30 11:13:15 +0200

    init

---
+++ Lab_10/Lab_10/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Lab_10
+{
+    class Program
+    {
+        static void Main()
+        {
+            Random r = new Random();
+            int max = Int32.MinValue;
+            int imax = 0;
+            int[] a = new int[8];
+
+            for (int i = 0; i < a.Length; i++)
+            {
+                Console.Write((a[i] = r.Next(-50, 50)) + " ");
+                if (max < a[i])
+                {
+                    max = a[i];
+                    imax = i;
+                }
+            }
+            for (int i = 0; i < a.Length; i++)
+                if (a[i] > 0)
+                {
+                 
+                    a[i] = a[imax];
+
+            }
+
+
+            Console.WriteLine();
+            for (int i = 0; i < a.Length; i++)
+                Console.Write(a[i] + " ");
+            Console.ReadKey();
+        }
+    }
+}
+
